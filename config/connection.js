c42f4223530025ffb2208319ca8defbeb3d70d40
Albert Tangtheinkul <bamf_albert@yahoo.com>
2018-01-08 00:46:02 -0800

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
+
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
 
 if (process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -15,8 +17,8 @@ if (process.env.JAWSDB_URL){
     connection = mysql.createConnection({
         host:'localhost',
         user:'root',
-        password:'hacktheplanet',
-        database: 'todoagain_db'
+        password:'',
+        database: 'burgers_db'
     });
 };
 
