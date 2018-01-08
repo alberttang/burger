c2bc7c23619a7f39f4c81dbcbc8fd5d702184966
Albert Tangtheinkul <bamf_albert@yahoo.com>
2018-01-08 00:09:41 -0800

    adding initial files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('whopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('double double', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
